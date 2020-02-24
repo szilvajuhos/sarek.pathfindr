5ccc18d9d9b5ef2ba76b4ee1bb89c711afbc94e5
Szilveszter Juhos <szilveszter.juhos@scilifelab.se>
2020-02-24 11:01:30 +0100

    Strelka somatic does something sensible

--- R/initRefs.R
+++ R/initRefs.R
@@ -24,7 +24,7 @@ reference_genome = 'GRCh38'
 baf = "the_BAF_file_bugger"
 logr = "the_logR_file_bugger"
 
-cat("Shaping tumor genes dataframe\n")
+tic("Shaping tumor genes dataframe")
 tumorgenes=data.table::fread('~/reports/reference_data/cancer_gene_census.csv',key='Gene Symbol')
 tumorgenes$chr=paste0('chr',str_replace(string=tumorgenes$`Genome Location`,pattern = ':.*',replacement = ''))
 temp=str_replace(string=tumorgenes$`Genome Location`,pattern = '.*:',replacement = '')
@@ -36,15 +36,17 @@ for (i in 1:nrow(chrsz)) {
   tumorgenes$cumstart[ix] <- tumorgenes$start[ix]+chrsz$starts[i]
   tumorgenes$cumend[ix] <- tumorgenes$end[ix]+chrsz$starts[i]
 }
-cat("Merging locals\n")
+toc()
+tic("Merging locals")
 local_tumorgenes = data.table::fread('~/reports/reference_data/2018_gene_list_tere_ref.csv',key='Gene')
 # get rid of rows without a gene name
 local_tumorgenes = subset.data.frame(local_tumorgenes,local_tumorgenes$Gene!='')
 # select only the gene names and the tier (a.k.a. score) 
 local_tumorgenes = local_tumorgenes[,1:2]
 alltumorgenes=unique(c(tumorgenes$`Gene Symbol`,local_tumorgenes$Gene))
-
-cat("Getting fusions\n")
+alltsg=tumorgenes[grep('TSG',tumorgenes$`Role in Cancer`),tumorgenes$`Gene Symbol`]
+toc()
+tic("Getting fusions")
 allfusion=tumorgenes[grep('fusion',`Role in Cancer`),`Gene Symbol`]
 allfusionpairs=NULL
 for (i in 1:length(allfusion)) {
@@ -52,8 +54,8 @@ for (i in 1:length(allfusion)) {
   if (length(t)>0) for (j in 1:length(t))
     allfusionpairs=c(allfusionpairs,paste(sort(c(allfusion[i],t[j])),collapse = ' '))
 }
-
-cat("Creating tiers\n")
+toc()
+tic("Creating tiers")
 # select tier 1 genes from both
 # then get a list of gene names as the union of the two sets
 tumorgenes_tier1 = subset.data.frame(tumorgenes,tumorgenes$Tier==1)
@@ -67,8 +69,31 @@ alltier1 = union(tumorgenes_tier1_genes,local_tumorgenes_tier1_genes)
 tumorgenes_tier2 = subset.data.frame(tumorgenes,tumorgenes$Tier==2)
 local_tumorgenes_tier2 = subset.data.frame(local_tumorgenes,local_tumorgenes$`Tier 1 and 2 for pediatric cancers final`==2)
 alltier2 = union(tumorgenes_tier2[,1],local_tumorgenes_tier2[,1])
-
-cosmic_fusions=fread('~/reports/reference_data/cosmic_fusions_table.csv',key = 'name')
-
 #alltier1=union(tumorgenes[Tier==1,tumorgenes$`Gene Symbol`],local_tumorgenes[`Tier 1 and 2 for pediatric cancers final`==1,local_tumorgenes$Gene])
 #alltier2=union(tumorgenes[Tier==2,tumorgenes$`Gene Symbol`],local_tumorgenes[`Tier 1 and 2 for pediatric cancers final`==2,local_tumorgenes$Gene])
+toc()
+tic("Reading COSMIC fusions")
+cosmic_fusions=fread('~/reports/reference_data/cosmic_fusions_table.csv',key = 'name')
+toc()
+
+tic("Reading SNV hotspots")
+hotspots_snv=unique(
+  fread('~/reports/reference_data/hotspots_v2_snv.csv')[,.(Hugo_Symbol,Amino_Acid_Position)])[-grep('splice',Amino_Acid_Position)]
+hotspots_snv$pos <- as.numeric(hotspots_snv$Amino_Acid_Position)
+toc()
+
+tic("Reading inframe hotspots")
+hotspots_inframe=unique(fread('~/reports/reference_data/hotspots_v2_inframe.csv')[,.(Hugo_Symbol,Amino_Acid_Position)])
+hotspots_inframe$start=as.numeric(str_replace(string = hotspots_inframe$Amino_Acid_Position,pattern = '-[0-9]*',replacement = ''))
+hotspots_inframe$end=as.numeric(str_replace(string = hotspots_inframe$Amino_Acid_Position,pattern = '[0-9]*-',replacement = ''))
+near_hotspots=NULL
+for (i in 1:nrow(hotspots_inframe)) 
+  near_hotspots=c(near_hotspots,
+                  paste(hotspots_inframe$Hugo_Symbol[i],
+                        c(seq(hotspots_inframe$start[i]-2,hotspots_inframe$start[i]+2),
+                          hotspots_inframe$end[i]-2,hotspots_inframe$end[i]+2)))
+for (i in 1:nrow(hotspots_snv))
+  near_hotspots=c(near_hotspots,
+                  paste(hotspots_snv$Hugo_Symbol[i],
+                        seq(hotspots_snv$pos[i]-2,hotspots_snv$pos[i]+2)))
+toc()
